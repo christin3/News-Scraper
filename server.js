7b0e4e5b3c9dc938ac60eed89bef1a201cdd4bcb
Christine Baba <17974025+christin3@users.noreply.github.com>
2018-11-28 14:17:07 -0600

    db working with scrape

--- server.js
+++ server.js
@@ -3,16 +3,15 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
-var rp = require('request-promise');
+//var request = require("request-promise");
 var PORT = process.env.PORT || 3000;
 // Requiring our Note and Article models
-var Note = require("/Users/christine/Coding /UT Bootcamp/Github/Reddit-Scraper/note.js");
-var Article = require("/Users/christine/Coding /UT Bootcamp/Github/Reddit-Scraper/article.js");
+//var Note = require("/Users/christine/Coding /UT Bootcamp/Github/Reddit-Scraper/models/Note.js");
+var Article = require("/Users/christine/Coding /UT Bootcamp/Github/Reddit-Scraper/models/Article.js");
 // Our scraping tools
-var request = require("request");
 var cheerio = require("cheerio");
-// Mongoose mpromise deprecated - use bluebird promises
 var Promise = require("bluebird");
+var request = require("request");
 
 mongoose.Promise = Promise;
 
@@ -53,45 +52,34 @@ app.get("/", function (req, res) {
 });
 
 // A GET request to scrape
-app.get("/scrape", function (req, res) {
+app.get("/scrape", function (req, result) {
 
     var subReddit = req.query.subReddit;
 
     // First, we grab the body of the html with request
-   rp("old.reddit.com/r/" + subReddit, function (error, response, html) {
+    request("https://old.reddit.com/r/" + subReddit, function (error, response, html) {
         // Then, we load that into cheerio and save it to $ for a shorthand selector
         var $ = cheerio.load(html);
         // Now, we grab every h2 within an article tag, and do the following:
         $("p.title").each(function (i, element) {
-
             // Save an empty result object
             var result = {};
-
             // Add the text and href of every link, and save them as properties of the result object
             result.title = $(this).text();
             result.link = $(this).children("a").attr("href");
-
-
             var entry = new Article(result);
-
             // Now, save that entry to the db
             entry.save(function () {
-
             });
-
         });
-    }).then(function() {
-       res.redirect('/')
-   })
-       .catch(function (err) {
-          console.log(err);
-       });
-
+    }).then(function () {
+        result.redirect('./')
+    }).catch(function (err) {
+            console.log(err);
+        });
 
 });
-
 //articles is being undefined,
-
 // This will get the articles we scraped from the mongoDB
 app.get("/articles", function (req, res) {
     // Grab every doc in the Articles array
@@ -155,8 +143,8 @@ app.post("/articles/:id", function (req, res) {
                 });
         }
     });
-});
-
+});//     });
+// });
 
 app.delete("/articles/:id", function (req, res) {
     //remove the note from the collection
@@ -189,6 +177,7 @@ app.delete("/articles/:id", function (req, res) {
     });
 
 });
+
 
 
 
